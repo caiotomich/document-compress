cc5a2d27c21bf136addae12c1e5816453252bdd8
Caio Tomich <caiotomich@gmail.com>
2022-05-26 11:12:20 -0300

    feat: return pdf after image compression

--- routes/buffer.js
+++ routes/buffer.js
@@ -3,6 +3,9 @@ const multer = require("multer");
 const sharp = require("sharp");
 const fs = require("fs");
 const { exit } = require('process');
+const PDFDocument = require('pdfkit');
+const stream = require('./stream');
+const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -10,22 +13,42 @@ const upload = multer({ storage });
 
 router.post("/buffer", upload.single('file'), async (req, res) => {
   const { buffer, originalname, size } = req.file;
+  const uuid = uuidv4();
+
+  localUStoreImage = './storage/uploads';
+  localStorePdf = './storage/pdfs';
 
   await sharp(buffer)
-  .webp({
-      quality: 60
-  })
+  .jpeg({ quality: 40 })
   .resize({
     height: 1720,
     withoutEnlargement: true
   })
-  .toBuffer({resolveWithObject: true})
-  .then(async buffer => {
-    buffer.success = true;
-    buffer.info.oldSize = size;
-    buffer.info.compress = ((buffer.info.size / size) -1) * -100;
+  .toFile(`${localUStoreImage}/output-${uuid}.jpeg`)
+  .then(info => {
+    const doc = new PDFDocument();
+    let docStream = new stream.WritableBufferStream();
 
-    return res.json(buffer);
+    doc.pipe(docStream);
+    doc.image(`${localUStoreImage}/output-${uuid}.jpeg`, 10, 10, {
+      fit: [doc.page.width - 20, doc.page.height - 20],
+      align: 'center'
+    });
+    doc.end()
+
+    info.formatImage = info.format
+    info.format = 'pdf'
+    info.compression = ((info.size / size) -1) * -100
+  
+    const buffer = {
+      success: true,
+      compress: info
+    }
+
+    docStream.on('finish', () => {
+      buffer.data = docStream.toBuffer()
+      return res.json(buffer)
+    })
   })
   .catch(err => {
     console.log('Error:', err);
